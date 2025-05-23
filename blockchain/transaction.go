a90c613c96e3ece2b6e8a04a3e2efe7156437424
ist1103898 <francisco.eira.leitao@tecnico.ulisboa.pt>
2025-05-23 19:37:13 +0200

    refactor: move transaction IO to separate file

--- blockchain/transaction.go
+++ blockchain/transaction.go
@@ -9,78 +9,62 @@ import (
 	"log"
 )
 
-// Transaction groups inputs and outputs, tracked by an ID.
 type Transaction struct {
 	ID      []byte
 	Inputs  []TxInput
 	Outputs []TxOutput
 }
 
-// TxOutput represents coins that can be claimed using a specific key.
-type TxOutput struct {
-	Value  int
-	PubKey string
-}
-
-// TxInput refers to a previous output being used in a new transaction.
-type TxInput struct {
-	ID  []byte
-	Out int
-	Sig string
-}
-
-// SetID hashes the entire transaction structure to set a unique ID.
 func (tx *Transaction) SetID() {
-	var buffer bytes.Buffer
-	encoder := gob.NewEncoder(&buffer)
-	err := encoder.Encode(tx)
+	var encoded bytes.Buffer
+	var hash [32]byte
+
+	encode := gob.NewEncoder(&encoded)
+	err := encode.Encode(tx)
 	Handle(err)
 
-	hash := sha256.Sum256(buffer.Bytes())
+	hash = sha256.Sum256(encoded.Bytes())
 	tx.ID = hash[:]
 }
 
-// CoinbaseTx creates the first transaction in a block, awarding coins.
-func CoinbaseTx(recipient, customData string) *Transaction {
-	if customData == "" {
-		customData = fmt.Sprintf("Reward to %s", recipient)
+func CoinbaseTx(to, data string) *Transaction {
+	if data == "" {
+		data = fmt.Sprintf("Coins to %s", to)
 	}
 
-	input := TxInput{ID: []byte{}, Out: -1, Sig: customData}
-	output := TxOutput{Value: 100, PubKey: recipient}
+	txin := TxInput{[]byte{}, -1, data}
+	txout := TxOutput{100, to}
 
-	coinbase := Transaction{
-		ID:      nil,
-		Inputs:  []TxInput{input},
-		Outputs: []TxOutput{output},
-	}
-	coinbase.SetID()
+	tx := Transaction{nil, []TxInput{txin}, []TxOutput{txout}}
+	tx.SetID()
 
-	return &coinbase
+	return &tx
 }
 
-// NewTransaction creates a standard transaction with inputs and outputs.
-func NewTransaction(sender, recipient string, amount int, bc *BlockChain) *Transaction {
+func NewTransaction(from, to string, amount int, chain *BlockChain) *Transaction {
 	var inputs []TxInput
 	var outputs []TxOutput
 
-	total, validOuts := bc.FindSpendableOutputs(sender, amount)
-	if total < amount {
-		log.Panic("Insufficient funds")
+	acc, validOutputs := chain.FindSpendableOutputs(from, amount)
+
+	if acc < amount {
+		log.Panic("Error: not enough funds")
 	}
 
-	for txIDStr, outs := range validOuts {
-		txID, err := hex.DecodeString(txIDStr)
+	for txid, outs := range validOutputs {
+		txID, err := hex.DecodeString(txid)
 		Handle(err)
 
-		for _, outIdx := range outs {
-			inputs = append(inputs, TxInput{ID: txID, Out: outIdx, Sig: sender})
+		for _, out := range outs {
+			input := TxInput{txID, out, from}
+			inputs = append(inputs, input)
 		}
 	}
 
-	outputs = append(outputs, TxOutput{Value: amount, PubKey: recipient})
-	if total > amount {
-		outputs = append(outputs, TxOutput{Value: total - amount, PubKey: sender})
+	outputs = append(outputs, TxOutput{amount, to})
+
+	if acc > amount {
+		outputs = append(outputs, TxOutput{acc - amount, from})
 	}
 
 	tx := Transaction{nil, inputs, outputs}
@@ -89,17 +73,6 @@ func NewTransaction(sender, recipient string, amount int, bc *BlockChain) *Trans
 	return &tx
 }
 
-// IsCoinbase returns true if the transaction is a coinbase (mining reward).
 func (tx *Transaction) IsCoinbase() bool {
 	return len(tx.Inputs) == 1 && len(tx.Inputs[0].ID) == 0 && tx.Inputs[0].Out == -1
-}
-
-// CanUnlock determines if the input signature matches the unlocking key.
-func (in *TxInput) CanUnlock(key string) bool {
-	return in.Sig == key
-}
-
-// CanBeUnlocked checks if the provided key can unlock the output.
-func (out *TxOutput) CanBeUnlocked(key string) bool {
-	return out.PubKey == key
 }
